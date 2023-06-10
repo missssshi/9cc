425fb535b2427124cbac4e0b85e6f3daf2e655e0
missssshi <missssshi@gmail.com>
2023-06-10 15:14:29 +0900

    23/6/10 step7 比較演算子

--- test.sh
+++ test.sh
@@ -26,4 +26,25 @@ assert 15 '5*(9-6)'
 assert 4 '(3+5)/2'
 assert 15 '-3 * -5'
 
+assert 0 "0==1"
+assert 1 "1==1"
+assert 0 "1!=1"
+assert 1 "0!=1"
+
+assert 0 "1<=0"
+assert 1 "0<=1"
+assert 1 "0<=0"
+
+assert 0 "1<0"
+assert 1 "0<1"
+assert 0 "0<0"
+
+assert 1 "1>=0"
+assert 0 "0>=1"
+assert 1 "0>=0"
+
+assert 1 "1>0"
+assert 0 "0>1"
+assert 0 "0>0"
+
 echo OK
